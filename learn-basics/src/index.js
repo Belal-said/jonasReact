57acfd59e76239a4fd3b145e2cd06fee68f0c2c2
belal-said <belalelkholy33@gmail.com>
2026-07-21 16:25:24 +0300

    Working with props in components

--- learn-basics/src/index.js
+++ learn-basics/src/index.js
@@ -1,9 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "./index.css";
 
 function App() {
   return (
-    <div>
+    <div className="container">
       <Header />
       <Menu />
       <Footer />
@@ -11,36 +12,51 @@ function App() {
   );
 }
 
-function Pizza() {
+function Menu() {
   return (
-    <div>
-      <img src="pizza/spinaci.jpg" alt="Pizza spinaci" />
-      <h2>Pizza</h2>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    <main className="menu">
+      <h2>Our menu</h2>
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizza/spinaci.jpg"
+        price={10}
+      />
+      <Pizza
+        name="Pizza Funghi"
+        ingredients="Tomato, mushrooms"
+        price={12}
+        photoName="pizza/funghi.jpg"
+      />
+    </main>
+  );
+}
+
+function Pizza(props) {
+  console.log(props);
+  return (
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+      </div>
     </div>
   );
 }
 
 function Header() {
-  return <h1>Fast React Pizza Co.</h1>
-}
-
-function Menu() {
   return (
-    <div>
-      <h2>Our menu</h2>
-      <Pizza />
-      <Pizza />
-      <Pizza />
-      <Pizza />
+    <div className="header">
+      <h1>Fast React Pizza Co.</h1>
     </div>
-  )
+  );
 }
 
 function Footer() {
   return (
     <footer>{new Date().toLocaleDateString()}. We're currrenly open</footer>
-  )
+  );
 }
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
